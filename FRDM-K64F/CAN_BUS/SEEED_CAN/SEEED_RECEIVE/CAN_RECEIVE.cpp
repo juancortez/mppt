cecfc3c83f2b1fce51e9a48c96ffa63bea5d3e9a
Juan Cortez <JCortez@us.ibm.com>
2015-10-23 16:15:08 -0500

    Changed how the CAN_Message gets printed and refactored some code for easier readability.

--- FRDM-K64F/CAN_BUS/SEEED_CAN/SEEED_RECEIVE/CAN_RECEIVE.cpp
+++ FRDM-K64F/CAN_BUS/SEEED_CAN/SEEED_RECEIVE/CAN_RECEIVE.cpp
@@ -8,7 +8,7 @@
 * Author: Juan Cortez
 * Team: Angus Ranson, Muhammad Bukhari, Rana Madkour, Josh Frazor, Zach Pavlich
 * Created on: October 20, 2015 at 16:05
-* Revised on: October 21, 2015 at 12:44
+* Revised on: October 23, 2015 at 16:1
 * 
 * Serial Communication on MAC
 * 
@@ -21,7 +21,11 @@
 #include "mbed.h"
 #include "seeed_can.h"
 
+// function that gets called on a receiving interrupt witht he specified ID
 void CAN_Interrupt_Received(void);
+
+// prints out the status of the CAN Bus initialization
+void printStatus(int);
 
 SEEED_CAN can(SEEED_CAN_CS,SEEED_CAN_IRQ, SEEED_CAN_MOSI, SEEED_CAN_MISO, SEEED_CAN_CLK , 500000);
 SEEED_CANMessage msg; // create empty CAN message
@@ -36,11 +40,7 @@ int main() {
     printf("SEEED_RECEIVE Program Starting...\r\n");
     
     int can_open_status = can.open(500000, SEEED_CAN::Normal);  // initialize CAN-BUS Shield
-    if(can_open_status){
-        printf("CAN Bus Shield successfully initialized!\r\n");    
-    } else{
-        printf("CAN BUS Shield initialization failed...\r\n");    
-    }
+    printStatus(can_open_status);
     
     //TODO: figure out which unique ID we want to use on the receiving side
     can.mask(0, 0x1FFFFFFF); // Configure Mask 0 to check all bits of a Standard CAN message Id
@@ -56,6 +56,15 @@ int main() {
   }
 }
 
+/* Prints the status of CAN_BUS initialization */
+void printStatus(int status){
+     if(status == 1){
+        printf("CAN Bus Shield successfully initialized!\r\n");  
+    } else{
+        printf("CAN BUS Shield initialization failed...\r\n");    
+    }
+}
+
 /*
 * This function is called when the receiver receives a message from a transmitting CAN-BUS. Since the filter is set, it will
 * only accept messages with the ID as specified in the can.filter parameter. In this program, it is 7. 
@@ -66,10 +75,12 @@ void CAN_Interrupt_Received(void){
       printf("Message received!\r\n");
       printf("The id is: %d.\r\n", msg.id);
       printf("The length of the message is: %d.\r\n", msg.len);
+      printf("The message received is: \r\n");
       for(counter = 0; counter < msg.len; counter++){
         //TODO: what are we going to do with this data?
-        printf("msg[%d] = %c\r\n", counter, msg.data[counter]);
+        printf("%c", msg.data[counter]);
       }
+      printf("\r\n");
       led2 = !led2; // Yellow toggle receive status LED
     }    
 }
