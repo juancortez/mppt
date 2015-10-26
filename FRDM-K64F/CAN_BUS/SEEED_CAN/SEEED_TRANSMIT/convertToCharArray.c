b30b5ceb1487039cc6249de8557f8f38fedf8dc3
Juan Cortez <JCortez@us.ibm.com>
2015-10-26 11:13:46 -0500

    C file used for debugging.

--- FRDM-K64F/CAN_BUS/SEEED_CAN/SEEED_TRANSMIT/convertToCharArray.c
+++ FRDM-K64F/CAN_BUS/SEEED_CAN/SEEED_TRANSMIT/convertToCharArray.c
@@ -2,9 +2,12 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 void printData(char*);
 void convertToCharArray(char*, float);
+float convertToVariable(char*);
+#define MESSAGE_LENGTH 8
 
 int main(void){
 	float test1 = 23.2345;
@@ -14,35 +17,60 @@ int main(void){
 	float test5 = 1980.2343123;
 	char buffer[8] = {}; // initialize an empty data buffer
 
-	char *ptr = buffer;
-	convertToCharArray(ptr, test1);
-	ptr = buffer;
-	printData(ptr);
-	memset(&buffer[0], 0, sizeof(buffer)); // empty array
+	// char *ptr = buffer;
+	// convertToCharArray(ptr, test1);
+	// ptr = buffer;
+	// printData(ptr);
+	// memset(&buffer[0], 0, sizeof(buffer)); // empty array
 
-	ptr = buffer;
-	convertToCharArray(ptr, test2);
-	ptr = buffer;
-	printData(ptr);
-	memset(&buffer[0], 0, sizeof(buffer)); // empty array
+	// ptr = buffer;
+	// convertToCharArray(ptr, test2);
+	// ptr = buffer;
+	// printData(ptr);
+	// memset(&buffer[0], 0, sizeof(buffer)); // empty array
 
-	ptr = buffer;
-	convertToCharArray(ptr, test3);
-	ptr = buffer;
-	printData(ptr);
-	memset(&buffer[0], 0, sizeof(buffer)); // empty array
+	// ptr = buffer;
+	// convertToCharArray(ptr, test3);
+	// ptr = buffer;
+	// printData(ptr);
+	// memset(&buffer[0], 0, sizeof(buffer)); // empty array
 
-	ptr = buffer;
-	convertToCharArray(ptr, test4);
-	ptr = buffer;
-	printData(ptr);
-	memset(&buffer[0], 0, sizeof(buffer)); // empty array
+	// ptr = buffer;
+	// convertToCharArray(ptr, test4);
+	// ptr = buffer;
+	// printData(ptr);
+	// memset(&buffer[0], 0, sizeof(buffer)); // empty array
 
-	ptr = buffer;
-	convertToCharArray(ptr, test5);
-	ptr = buffer;
-	printData(ptr);
+	// ptr = buffer;
+	// convertToCharArray(ptr, test5);
+	// ptr = buffer;
+	// printData(ptr);
+	// memset(&buffer[0], 0, sizeof(buffer)); // empty array
+
+	convertToCharArray(&buffer[0], test1);
+	float ans = convertToVariable(&buffer[0]);
+	printf("Answer is: %f\n", ans);
+
 	memset(&buffer[0], 0, sizeof(buffer)); // empty array
+	convertToCharArray(&buffer[0], test2);
+	ans = convertToVariable(&buffer[0]);
+	printf("Answer is: %f\n", ans);
+
+	memset(&buffer[0], 0, sizeof(buffer)); // empty array
+	convertToCharArray(&buffer[0], test3);
+	ans = convertToVariable(&buffer[0]);
+	printf("Answer is: %f\n", ans);
+
+	memset(&buffer[0], 0, sizeof(buffer)); // empty array
+	convertToCharArray(&buffer[0], test4);
+	ans = convertToVariable(&buffer[0]);
+	printf("Answer is: %f\n", ans);
+
+	memset(&buffer[0], 0, sizeof(buffer)); // empty array
+	convertToCharArray(&buffer[0], test5);
+	ans = convertToVariable(&buffer[0]);
+	printf("Answer is: %f\n", ans);
+
 
 	float mpptReadings[6] = {test1, test2, test3, test4, test5};
 	
@@ -87,3 +115,29 @@ void convertToCharArray(char* ptr, float val){
  		expandedVal /= 10;
 	}
 }
+
+/*
+* This function converts an 8 char array variable into a float variable
+* and returns it.
+*
+* Expected Result: [0, 2, 3, ., 2, 3, 4] -> 23.234
+*/
+float convertToVariable(char *ptr){
+	float hundreds = ((float)*ptr++ - 48) * 100;
+	hundreds = hundreds < 0 ? 0 : hundreds;
+	float tens = ((float)*ptr++ - 48) * 10;
+	tens = tens < 0 ? 0 : tens;
+	float ones = ((float)*ptr++ - 48) * 1;
+	ones = ones < 0 ? 0 : ones;
+	ptr++; // skip over decimal value
+	float tenths = ((float)*ptr++ - 48) / 10;
+	tenths = tenths < 0 ? 0 : tenths;
+	float hundredths = ((float)*ptr++ - 48) / 100;
+	hundredths = hundredths < 0 ? 0 : hundredths;
+	float thousandths = ((float)*ptr++ - 48) / 1000;
+	thousandths = thousandths < 0 ? 0 : thousandths;
+	return hundreds+tens+ones+tenths+hundredths+thousandths;
+}
+
+
+
